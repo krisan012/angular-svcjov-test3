c94b59535bc48ad941367c593fb4c75cbe5add8e
krisan012 <matuazon43@gmail.com>
2023-04-03 13:42:33 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  cards = [
-    {
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      age: 20,
-      gender: 'Male',
-      telephone: '+299999',
-      duplicates: 0 //for tracking duplicate count
-    }
-  ];
+  cards: any[] = [];
+
+  ngOnInit(){
+    this.fetchData();
+  }
 
   constructor(private http: HttpClient) {}
 
-  duplicateCard(card: any): void{
+  duplicateCard(card: any): void {
     console.log('duplicate');
     const newCard = { ...card, duplicates: 0 };
     newCard.name = `${card.name}#${card.duplicates + 1}`; //new name of duplicated card
     card.duplicates++; //increase duplicate count
 
     this.cards.push(newCard);
+  }
+
+  fetchData(): void {
+    this.http
+      .get('https://dummyjson.com/users?count=10')
+      .subscribe((response: any) => {
+        this.cards = response.users.slice(0,10).map((user: any) => {
+          return {
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.email,
+            age: user.age,
+            gender: user.gender,
+            telephone: user.phone,
+            duplicates: 0,
+          };
+        });
+      });
+  }
+
+  deleteCard(index: number): void {
+    console.log(index)
+    this.cards.splice(index, 1);
   }
 }
